95417017338eb4ec152077090dee9d3e771eeab9
kanmuteh1 <jameskgayejr@gmail.com>
2022-01-14 13:54:17 +0000

    testing out heroku

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request
+
+# Configure application
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html", name=request.args.get("name", "world"))
+
+@app.route("/greet")
+def greet():
+    return render_template("greet.html", name=request.args.get("name", "world"))
